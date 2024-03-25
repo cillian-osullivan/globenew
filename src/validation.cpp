40380746142f9d93ba429aeb7f99ca58dd662e37
Cillian O’ Sullivan <81384331+cillian-osullivan@users.noreply.github.com>
2024-03-25 15:28:26 +0000

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -136,6 +136,8 @@ GlobalMutex g_best_block_mutex;
 std::condition_variable g_best_block_cv;
 uint256 g_best_block;
 bool g_parallel_script_checks{false};
+bool fAddressIndex = false; // qtum
+bool fLogEvents = false;
 bool fCheckBlockIndex = false;
 bool fCheckpointsEnabled = DEFAULT_CHECKPOINTS_ENABLED;
 unsigned int MIN_BLOCKS_TO_KEEP = 288;
